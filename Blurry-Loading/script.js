56d0903eb01a79fbf92a545ef8b6989e91616030
DevDhaif <devdhaif@gmail.com>
2021-03-28 23:22:12 +0200

    hide percentage

--- Blurry-Loading/script.js
+++ Blurry-Loading/script.js
@@ -9,9 +9,14 @@ function blurring(){
     }
 
     loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    loadText.style.opacity = scale(load, 0.5, 100, 1, 0)
+    if(loadText.style.opacity==0){
+        loadText.style.display='none'
+    }
+    
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
   }
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
   }
+  
